e7a3d4fafc5a3eb1939f1b7705bee4d105288cb0
Philippe Ngom <philngom@gmail.com>
2022-02-18 12:50:21 -0800

    added useFetch to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import { logOut, getUser } from './services/fetch-utils.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AuthPage from './AuthPage.js';
 
 function App() {
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    async function fetchUser() {
+      const signedInUser = await getUser();
+      setUser(signedInUser);
+    }
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <AuthPage />
+      <header>
+        {
+          user &&
+          <button>Logout</button>
+        }
       </header>
+      <main>
+        <AuthPage />
+      </main>
     </div>
   );
 }
