1810bafd1ff1d17b93c784a62f0c61ff179c5d67
Philippe Ngom <philngom@gmail.com>
2022-02-18 11:09:41 -0800

    finished signIn and signOut functions in fetch-utils

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TESTING<h1/>
+        <h1>TESTING</h1>
       </header>
     </div>
   );
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,26 @@
+import { client, checkError } from './client.js';
+
+export function getUser() {
+  return client.auth.session();
+}
+
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({
+    email,
+    password
+  });
+  return response.user;
+}
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({
+    email,
+    password
+  });
+  return response.user;
+}
+
+export async function logOut() {
+  await client.auth.signOut();
+  return window.location.href = '../';
+}
+
