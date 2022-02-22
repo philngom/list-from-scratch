280e449c8b703cb8b31436cb245371778a00b732
Philippe Ngom <philngom@gmail.com>
2022-02-22 00:05:16 -0800

    added search endpoint

---
+++ functions/search.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+exports.handler = async (event) => {
+  const searchQuery = event.queryStringParameters.search;
+
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${searchQuery}`);
+    const data = await response.json();
+    const json = JSON.stringify(data);
+
+    return {
+      statusCode: 200,
+      body: json
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
